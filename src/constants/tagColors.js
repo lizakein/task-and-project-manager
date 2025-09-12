ab9e51c2a2d564900e091325b8fe32f0b2a40902
lizakein <LizaKein@mail.ru>
2025-09-12 18:29:53 +0300

    Create the color dictionary

---
+++ src/constants/tagColors.js
@@ -0,0 +1,7 @@
+export const TAG_COLORS = {
+  blue: { text: "#4970D5", background: "#4970D520"},
+  red: { text: "#D8727D", background: "#D8727D20"},
+  green: { text: "#68B266", background: "#68B26620"},
+  orange: { text: "#D58D49", background: "#D58D4920"},
+  pink: { text: "#c549d5ff", background: "#c549d523"}
+}
