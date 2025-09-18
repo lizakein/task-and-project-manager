32895a2c98cecceeacefcae6c4d7fed4b54dbffa
lizakein <LizaKein@mail.ru>
2025-09-18 16:46:33 +0300

    Convert tagUtils.js to typescript

--- src/utils/tagUtils.ts
+++ src/utils/tagUtils.ts
@@ -1,6 +1,6 @@
 import { TAG_COLORS } from "@constants/tagColors"
 
-export function getTagStyle(colorKey) {
+export function getTagStyle(colorKey: keyof typeof TAG_COLORS) {
   const color = TAG_COLORS[colorKey];
 
   return {
