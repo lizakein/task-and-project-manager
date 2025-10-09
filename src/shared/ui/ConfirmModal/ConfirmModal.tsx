46be14256253903ef28a0d23c046ee11917d036a
lizakein <lizakein@mail.ru>
2025-10-09 18:23:02 +0300

    Improve accessibility in ConfirmModal

--- src/shared/ui/ConfirmModal/ConfirmModal.tsx
+++ src/shared/ui/ConfirmModal/ConfirmModal.tsx
@@ -1,5 +1,6 @@
 import { createPortal } from "react-dom";
 import './ConfirmModal.css';
+import { useEffect, useRef } from "react";
 
 interface ConfirmModalProps {
   isOpen: boolean;
@@ -12,16 +13,38 @@ interface ConfirmModalProps {
 export function ConfirmModal({ isOpen, title, message, onConfirm, onCancel}: ConfirmModalProps) {
   if (!isOpen) return null;
 
+  const confirmButtonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    confirmButtonRef.current?.focus();
+  }, []);
+
   return createPortal(
     <div className="overlay confirm-modal__overlay">
-      <div className="window confirm-modal__window">
-        <h2 className="confirm-modal__title">{title}</h2>
-        <p className="confirm-modal__message">{message}</p>
+      <div 
+        className="window confirm-modal__window"
+        role="alertdialog"
+        aria-modal="true"
+        aria-labelledby="confirm-title"
+        aria-describedby="confirm-message"
+      >
+        <h2 id="confirm-title" className="confirm-modal__title">
+          {title}
+        </h2>
+        <p id="confirm-message"className="confirm-modal__message">
+          {message}
+        </p>
+
         <div className="confirm-modal__actions">
-          <button className="button button--warning" onClick={onConfirm}>
+          <button 
+            ref={confirmButtonRef}
+            type="button" 
+            className="button button--warning" 
+            onClick={onConfirm}
+          >
             Yes, I'm sure
           </button>
-          <button className="button button--primary" onClick={onCancel}>
+          <button type="button" className="button button--primary" onClick={onCancel}>
             No
           </button>
         </div>     
