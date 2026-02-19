7762b0ee2882901bc532bff4eede61549b66c704
lizakein <lizakein@mail.ru>
2026-02-19 14:10:53 +0300

    Delete unused code from TaskOptions

--- src/features/tasks/components/TaskOptions.tsx
+++ src/features/tasks/components/TaskOptions.tsx
@@ -1,10 +1,10 @@
 import { useNavigate } from "react-router-dom";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { useStore } from "@store/useStore";
 import { OptionsWindow } from "@ui/OptionsWindow/OptionsWindow";
 import { ConfirmModal } from "@ui/ConfirmModal/ConfirmModal";
-import EditIcon from '@assets/icons/actions/edit-icon.svg';
-import TrashIcon from '@assets/icons/actions/trash-icon.svg';
+import EditIcon from "@assets/icons/actions/edit-icon.svg";
+import TrashIcon from "@assets/icons/actions/trash-icon.svg";
 import { MenuPosition } from "@hooks/useContextMenu";
 
 interface TaskOptionsProps {
@@ -14,12 +14,19 @@ interface TaskOptionsProps {
   title: string | null;
   onClose: () => void;
   triggerRef: React.RefObject<HTMLButtonElement | null>;
-};
+}
 
-export function TaskOptions({ menuPosition, projectId, openId, title, onClose, triggerRef }: TaskOptionsProps) {
-  const [ isModalOpen, setIsModalOpen ] = useState(false);
+export function TaskOptions({
+  menuPosition,
+  projectId,
+  openId,
+  title,
+  onClose,
+  triggerRef,
+}: TaskOptionsProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const navigate = useNavigate();
-  const deleteTask = useStore(state => state.deleteTask);
+  const deleteTask = useStore((state) => state.deleteTask);
 
   const handleEditTask = () => {
     navigate(`/project/${projectId}/${openId}`);
@@ -35,34 +42,38 @@ export function TaskOptions({ menuPosition, projectId, openId, title, onClose, t
     setIsModalOpen(true);
   };
 
-  return(
-    <OptionsWindow position={menuPosition} onClose={onClose} triggerRef={triggerRef}>
-      <button 
-        className='options-window__item'
-        role="menuitem" 
+  return (
+    <OptionsWindow
+      position={menuPosition}
+      onClose={onClose}
+      triggerRef={triggerRef}
+    >
+      <button
+        className="options-window__item"
+        role="menuitem"
         type="button"
         onClick={handleEditTask}
       >
         <img src={EditIcon} alt="" role="presentation" />
-        <span className='options-window__item-label'>Edit</span>
+        <span className="options-window__item-label">Edit</span>
       </button>
-      <button 
-        className='options-window__item'
-        role="menuitem" 
+      <button
+        className="options-window__item"
+        role="menuitem"
         type="button"
         onClick={handleDeleteClick}
       >
         <img src={TrashIcon} alt="" role="presentation" />
-        <span className='options-window__item-label red'>Trash</span>
+        <span className="options-window__item-label red">Trash</span>
       </button>
 
-      <ConfirmModal 
+      <ConfirmModal
         isOpen={isModalOpen}
         title="Delete task"
         message={`Are you sure you want to delete the task: ${title}`}
         onConfirm={handleConfirmDelete}
         onCancel={() => setIsModalOpen(false)}
       />
-    </OptionsWindow>  
+    </OptionsWindow>
   );
 }
