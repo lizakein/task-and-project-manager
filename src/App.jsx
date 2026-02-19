ee4d78e03c42a5e1b62de99e0330cd70643c852d
lizakein <lizakein@mail.ru>
2026-02-19 13:42:20 +0300

    Add eslint and prettier in project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
@@ -7,12 +8,12 @@ import { useStore } from "./store/useStore";
 import { ProjectPage } from "./pages/ProjectPage/ProjectPage";
 import { Homepage } from "./pages/Homepage/Homepage";
 import { EditTaskPage } from "./pages/EditTaskPage/EditTaskPage";
-import './App.css';
 import { CalendarPage } from "./pages/CalendarPage/CalendarPage";
 import { AnalyticsPage } from "./pages/AnalyticsPage/AnalyticsPage";
 import { SettingsPage } from "./pages/SettingsPage/SettingsPage";
 import { NotFoundPage } from "./pages/NotFoundPage/NotFoundPage";
 import { ProjectsPage } from "./pages/ProjectsPage/ProjectsPage";
+import "./App.css";
 
 function App() {
   const { loadProjects, loadTasks } = useStore();
@@ -22,7 +23,6 @@ function App() {
     loadTasks();
   }, [loadProjects, loadTasks]);
 
-
   return (
     <DndProvider backend={HTML5Backend}>
       <Routes>
@@ -31,12 +31,12 @@ function App() {
         <Route path="/analytics" element={<AnalyticsPage />} />
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/project" element={<ProjectsPage />} />
-        <Route path="/project/:projectId" element={<ProjectPage />} />  
-        <Route path="/project/:projectId/:taskId" element={<EditTaskPage />} /> 
+        <Route path="/project/:projectId" element={<ProjectPage />} />
+        <Route path="/project/:projectId/:taskId" element={<EditTaskPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </DndProvider>
   );
 }
 
-export default App
+export default App;
