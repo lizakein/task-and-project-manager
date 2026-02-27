e24b302a3aa593bed7aee55c1b269d8f67dfacfc
lizakein <lizakein@mail.ru>
2026-02-27 17:22:38 +0300

    Fix import App.js instead of App.tsx in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
