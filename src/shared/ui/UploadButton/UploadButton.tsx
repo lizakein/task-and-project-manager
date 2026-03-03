c836ec7a71ecf44a293e45fd29c204b9a553dcf4
lizakein <lizakein@mail.ru>
2026-03-03 17:25:17 +0300

    Delete type from Upload Button component

--- src/shared/ui/UploadButton/UploadButton.tsx
+++ src/shared/ui/UploadButton/UploadButton.tsx
@@ -3,11 +3,7 @@ import { Button, Icon } from "@ui/index";
 
 export function UploadButton() {
   return (
-    <Button
-      type="button"
-      className="upload-button"
-      leftIcon={<Icon src={UploadImgaeIcon} />}
-    >
+    <Button className="upload-button" leftIcon={<Icon src={UploadImgaeIcon} />}>
       <span className="button__text">Upload Image</span>
     </Button>
   );
