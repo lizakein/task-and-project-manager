fe412f2e81c2951e7c4d6c26710741d8218cf32d
lizakein <lizakein@mail.ru>
2026-03-03 17:03:17 +0300

    Refactor TaskFilter to use Button and Icon components

--- src/features/tasks/components/TaskFilter/TaskFilter.tsx
+++ src/features/tasks/components/TaskFilter/TaskFilter.tsx
@@ -3,6 +3,7 @@ import { useContextMenu } from "@hooks/useContextMenu";
 import FilterIcon from "@assets/icons/actions/filter-icon.svg";
 import ArrowDownIcon from "@assets/icons/actions/arrow-down-icon.svg";
 import ArrowUpIcon from "@assets/icons/actions/arrow-up-icon.svg";
+import { Button, Icon } from "@ui/index";
 import { TaskFilterMenu } from "./TaskFilterMenu";
 import "./TaskFilter.css";
 
@@ -19,20 +20,15 @@ export function TaskFilter({ contextMenu }: TaskFilterProps) {
 
   return (
     <>
-      <button
+      <Button
         ref={filterButtonRef}
-        type="button"
         className="task-controls__button"
         onClick={(e) => handleMoreClick(e, "filter")}
+        leftIcon={<Icon src={FilterIcon} />}
+        rightIcon={<Icon src={isOpen ? ArrowUpIcon : ArrowDownIcon} />}
       >
-        <img src={FilterIcon} alt="" role="presentation" />
         <span className="task-controls__button-name">Filter</span>
-        <img
-          src={isOpen ? ArrowUpIcon : ArrowDownIcon}
-          alt=""
-          role="presentation"
-        />
-      </button>
+      </Button>
 
       {isOpen && menuPosition && (
         <TaskFilterMenu
