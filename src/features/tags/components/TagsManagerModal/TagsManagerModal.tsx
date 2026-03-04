5878a0b60236fe63b762bce9122dbf1fc04da557
lizakein <lizakein@mail.ru>
2026-03-04 20:14:59 +0300

    Change import path and improve readability in TagsManagerModal

--- src/features/tags/components/TagsManagerModal/TagsManagerModal.tsx
+++ src/features/tags/components/TagsManagerModal/TagsManagerModal.tsx
@@ -1,21 +1,21 @@
 import { createPortal } from "react-dom";
-import { TagsManagerContent } from "./TagsManagerContent";
-import './TagsManagerModal.css';
+import { TagsManagerContent } from "../TagsManagerContent";
 import { FieldState } from "types/fieldState";
+import "./TagsManagerModal.css";
 
 interface TagsManagerModalProps {
-	isTagsModalOpen: FieldState<boolean>;
+  isTagsModalOpen: FieldState<boolean>;
 }
 
 export function TagsManagerModal({ isTagsModalOpen }: TagsManagerModalProps) {
-	if (!isTagsModalOpen.value) return null;
+  if (!isTagsModalOpen.value) return null;
 
-	return createPortal(
-		<div className="overlay tags-manager-modal__overlay">
-			<div className="window tags-manager-modal__window">
-				<TagsManagerContent setIsTagsModalOpen={isTagsModalOpen.setValue} />
-			</div>	
-		</div>,
-		document.body
-	);
+  return createPortal(
+    <div className="overlay tags-manager-modal__overlay">
+      <div className="window tags-manager-modal__window">
+        <TagsManagerContent setIsTagsModalOpen={isTagsModalOpen.setValue} />
+      </div>
+    </div>,
+    document.body
+  );
 }
