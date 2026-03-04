a0d40181a1e6f55ce47672761821e721b2e4b426
lizakein <lizakein@mail.ru>
2026-03-04 19:26:36 +0300

    Export OptionsWindow and ConfirmModal in index.ts

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,3 +2,5 @@ export { Button } from "./Button/Button";
 export { Icon } from "./Icon/Icon";
 export { IconButton } from "./IconButton/IconButton";
 export { ToggleButton } from "./ToggleButton/ToggleButton";
+export { OptionsWindow } from "./OptionsWindow/OptionsWindow";
+export { ConfirmModal } from "./ConfirmModal/ConfirmModal";
