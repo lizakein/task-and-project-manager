347c8d635add7493589b852f230c6a6e8790c033
lizakein <lizakein@mail.ru>
2026-03-04 18:05:35 +0300

    Refactor TagButton to use ToggleButton component

--- src/features/tasks/components/TagButton/TagButton.tsx
+++ src/features/tasks/components/TagButton/TagButton.tsx
@@ -1,12 +1,13 @@
 import { TAG_COLORS } from "@constants/tagColors";
 import { Tag } from "@features/tags";
+import { ToggleButton } from "@ui/index";
 import { getTagStyle } from "@utils/tagUtils";
 
 interface TagButtonProps {
   tag: Tag;
   selected: boolean;
   onClick: () => void;
-};
+}
 
 export function TagButton({ tag, selected, onClick }: TagButtonProps) {
   const { color, backgroundColor } = getTagStyle(
@@ -14,17 +15,18 @@ export function TagButton({ tag, selected, onClick }: TagButtonProps) {
   );
 
   return (
-    <button 
-      type="button" 
-      className={`tag tag--${tag.color} ${selected ? 'tag--selected' : ''}`}
-      style={{
-        "--tag-text": color,
-        "--tag-bg": backgroundColor
-      } as React.CSSProperties}
-      aria-pressed={selected}
+    <ToggleButton
+      className={`chip tag tag--${tag.color}`}
+      style={
+        {
+          "--tag-text": color,
+          "--tag-bg": backgroundColor,
+        } as React.CSSProperties
+      }
+      selected={selected}
       onClick={onClick}
     >
       {tag.label}
-    </button>
+    </ToggleButton>
   );
 }
