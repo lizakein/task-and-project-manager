db45129285f180e59b554029f73a07b4727b91a9
lizakein <lizakein@mail.ru>
2026-03-05 14:39:53 +0300

    Fix handleCreateProject in ProjectsPage to navigate to a new project

--- src/pages/ProjectsPage/ProjectsPage.tsx
+++ src/pages/ProjectsPage/ProjectsPage.tsx
@@ -2,17 +2,19 @@ import { useNavigate } from "react-router-dom";
 import { Layout } from "@layout/Layout/Layout";
 import { useProjectsStore } from "@store/hooks";
 import { Button } from "@ui/index";
+import { createProject } from "@utils/projectUtils";
 import "./ProjectsPage.css";
 
 export function ProjectsPage() {
   const navigate = useNavigate();
 
-  const { projects, addProject: addNewProject } = useProjectsStore();
+  const { projects, addProject } = useProjectsStore();
 
   const hasProjects = projects.length > 0;
 
   const handleCreateProject = () => {
-    const newProject = addNewProject();
+    const newProject = createProject();
+    addProject(newProject);
     navigate(`/project/${newProject.id}`);
   };
 
