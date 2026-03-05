e322b25b36f54142662d384bcb1e25bcc11fc74b
lizakein <lizakein@mail.ru>
2026-03-05 13:46:03 +0300

    Refactor NotFoundPage to use Layout

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,21 +1,22 @@
-import { Header } from "@layout/Header/Header";
+import { Layout } from "@layout/Layout/Layout";
 import "./NotFoundPage.css";
 
 export function NotFoundPage() {
   return (
-    <main className="page not-found-page" aria-labelledby="not-found-heading">
-      <Header />
-      <div className="content not-found-page__content">
-        <h2 id="not-found-heading" className="not-found-page__title">404</h2>
+    <Layout>
+      <div className="not-found-page__content">
+        <h2 id="not-found-heading" className="not-found-page__title">
+          404
+        </h2>
         <p className="not-found-page__message">Page not found</p>
-        <a 
-          href="/" 
-          className="button not-found-page__link" 
+        <a
+          href="/"
+          className="button not-found-page__link"
           aria-label="Go back to homepage"
         >
           Go to homepage
         </a>
       </div>
-    </main>
+    </Layout>
   );
 }
