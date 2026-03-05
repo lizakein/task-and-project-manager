84d9afefd351f31653e8b208511341cfdc1fce4b
lizakein <lizakein@mail.ru>
2026-03-05 18:45:05 +0300

    Add TASK_STATUS const

--- src/features/tasks/types.ts
+++ src/features/tasks/types.ts
@@ -1,5 +1,20 @@
+export const TASK_STATUS = {
+  todo: {
+    label: "To Do",
+    moves: ["done", "in-progress"],
+  },
+  "in-progress": {
+    label: "In Progress",
+    moves: ["todo", "done"],
+  },
+  done: {
+    label: "Done",
+    moves: ["in-progress", "todo"]
+  },
+} as const;
+
 export type Priority = "low" | "medium" | "high";
-export type Status = "todo" | "in-progress" | "done";
+export type Status = keyof typeof TASK_STATUS;
 
 export interface Task {
   id: string;
