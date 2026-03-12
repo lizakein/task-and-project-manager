2daa2aad5fefbe8111d6b7a2bb37be144256d91e
lizakein <lizakein@mail.ru>
2026-03-12 16:02:11 +0300

    Assemble a HomePage from components

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,16 @@
-import { DevelopmentPageLayout } from "@layout/DevelopmentLayout/DevelopmentPageLayout";
 import { Layout } from "@layout/Layout/Layout";
+import { Statistics } from "@features/statistics";
+import { MyTasks } from "@features/tasks";
+import { Calendar } from "@features/calendar";
 
 export function HomePage() {
   return (
     <Layout>
-      <DevelopmentPageLayout />
+      <main className="home-page__content">
+        <Statistics />
+        <MyTasks />
+        <Calendar />
+      </main>
     </Layout>
   );
 }
