4ba245f79a1492f8a870de233e57251b8be95905
lizakein <lizakein@mail.ru>
2026-03-12 18:57:41 +0300

    Add Calendar styles

--- src/features/calendar/components/Calendar.tsx
+++ src/features/calendar/components/Calendar.tsx
@@ -1,19 +1,22 @@
 import { Button } from "@ui/index";
 import { WeekDays } from "./WeekDays";
 import { Timeline } from "./Timeline";
+import "./Calendar.css";
 
 export default function Calendar() {
   const current_date = "Monday, September 1 2025";
 
   return (
     <section className="calendar" aria-labelledby="calendar-title">
-      <h2 id="calendar-title" className="calendar_title">
-        Calendar
-      </h2>
+      <div className="calendar__header">
+        <h2 id="calendar-title" className="calendar__title">
+          Calendar
+        </h2>
 
-      <Button className="calendar__link" variant="ghost">
-        See all
-      </Button>
+        <Button className="calendar__link" variant="ghost">
+          See all
+        </Button>
+      </div>
 
       <p className="calendar__date">{current_date}</p>
 
