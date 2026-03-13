b2561cec717b3d14bc1311da3de05949dfb93261
lizakein <lizakein@mail.ru>
2026-03-13 18:00:50 +0300

    Change StatisticsCard props

--- src/features/statistics/components/StatisticsCard.tsx
+++ src/features/statistics/components/StatisticsCard.tsx
@@ -1,14 +1,21 @@
 interface StatisticsCardProps {
   title: string;
-  value: string;
+  completedTasks: number;
+  allTasks: number;
 }
 
-export default function StatisticsCard({ title, value }: StatisticsCardProps) {
+export default function StatisticsCard({
+  title,
+  completedTasks,
+  allTasks,
+}: StatisticsCardProps) {
   return (
     <article className="statistics-card">
       <p className="statistics-card__title">{title}</p>
 
-      <p className="statistics-card__value">{value}</p>
+      <p className="statistics-card__value">
+        {completedTasks}/{allTasks}
+      </p>
     </article>
   );
 }
