0acf922bbfc4d1234702e9ac254e801e74b0d149
lizakein <lizakein@mail.ru>
2026-03-13 17:59:10 +0300

    Add getTasksStats method

---
+++ src/features/tasks/model/getTasksStats.ts
@@ -0,0 +1,32 @@
+import { Task } from "../types";
+import { isToday, isThisWeek, isThisMonth } from "@utils/dateFilters";
+
+export function getTasksStats(tasks: Task[]) {
+  return tasks.reduce(
+    (acc, task) => {
+      const date = new Date(task.dueDate);
+
+      if (isToday(date)) {
+        acc.today.total++;
+        if (task.status === "done") acc.today.completed++;
+      }
+
+      if (isThisWeek(date)) {
+        acc.week.total++;
+        if (task.status === "done") acc.week.completed++;
+      }
+
+      if (isThisMonth(date)) {
+        acc.month.total++;
+        if (task.status === "done") acc.month.completed++;
+      }
+
+      return acc;
+    },
+    {
+      today: { total: 0, completed: 0 },
+      week: { total: 0, completed: 0 },
+      month: { total: 0, completed: 0 },
+    }
+  );
+}
