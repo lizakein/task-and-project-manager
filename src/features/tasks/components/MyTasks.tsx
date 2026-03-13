1d7f17b4c428af2ca9dc19849c3f3a4511803891
lizakein <lizakein@mail.ru>
2026-03-13 19:54:16 +0300

    Replace static info in MyTasks component with dynamic ones

--- src/features/tasks/components/MyTasks.tsx
+++ src/features/tasks/components/MyTasks.tsx
@@ -1,7 +1,19 @@
+import { useTasksStore } from "@store/hooks";
 import { TaskItem } from "./TaskItem";
 import "./MyTasks.css";
 
 export default function MyTasks() {
+  const { tasks } = useTasksStore();
+
+  const filteredTasks = tasks.filter((task) => task.status !== "done");
+
+  const sortedTasks = [...filteredTasks].sort((a, b) => {
+    const valueA = a.dueDate ? new Date(a.dueDate).getTime() : Infinity;
+    const valueB = b.dueDate ? new Date(b.dueDate).getTime() : Infinity;
+
+    return valueA < valueB ? -1 : 1;
+  });
+
   return (
     <section className="my-tasks" aria-labelledby="my-tasks-title">
       <h2 id="my-tasks-title" className="my-tasks__title">
@@ -9,9 +21,9 @@ export default function MyTasks() {
       </h2>
 
       <ul className="my-tasks__list">
-        <TaskItem title="Task 1" project="Project 1" deadline="28.06.25" />
-        <TaskItem title="Task 2" project="Project 1" deadline="29.06.25" />
-        <TaskItem title="Task 3" project="Project 2" deadline="30.06.25" />
+        {sortedTasks.map((task) => (
+          <TaskItem key={task.id} task={task} />
+        ))}
       </ul>
     </section>
   );
