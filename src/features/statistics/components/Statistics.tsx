4dc0512e05cf5012c0ba16584a369954a271dc54
lizakein <lizakein@mail.ru>
2026-03-13 18:02:39 +0300

    Replace static values in the Statistics component with dynamic ones

--- src/features/statistics/components/Statistics.tsx
+++ src/features/statistics/components/Statistics.tsx
@@ -1,20 +1,38 @@
+import { useMemo } from "react";
+import { useTasksStore } from "@store/hooks";
+import { getTasksStats } from "@features/tasks/model/getTasksStats";
 import StatisticsCard from "./StatisticsCard";
 import "./Statistics.css";
 
 export default function Statistics() {
+  const { tasks } = useTasksStore();
+  const stats = useMemo(() => getTasksStats(tasks), [tasks]);
+
   return (
     <section className="statistics">
       <ul className="statistics__list">
         <li className="statistics__item">
-          <StatisticsCard title="Today todos" value="2/5" />
+          <StatisticsCard
+            title="Today todos"
+            completedTasks={stats.today.completed}
+            allTasks={stats.today.total}
+          />
         </li>
 
         <li className="statistics__item">
-          <StatisticsCard title="Week todos" value="8/18" />
+          <StatisticsCard
+            title="Week todos"
+            completedTasks={stats.week.completed}
+            allTasks={stats.week.total}
+          />
         </li>
 
         <li className="statistics__item">
-          <StatisticsCard title="Month todos" value="22/57" />
+          <StatisticsCard
+            title="Month todos"
+            completedTasks={stats.month.completed}
+            allTasks={stats.month.total}
+          />
         </li>
       </ul>
     </section>
