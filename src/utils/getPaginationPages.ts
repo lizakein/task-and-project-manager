ec81591fd4298a77652d91cee8fad8730c4528ee
lizakein <lizakein@mail.ru>
2026-03-14 16:16:05 +0300

    Create getPaginationPages util

---
+++ src/utils/getPaginationPages.ts
@@ -0,0 +1,29 @@
+export function getPaginationPages(currentPage: number, totalPages: number) {
+  const pages: (number | "...")[] = [];
+
+  if (totalPages <= 5) {
+    for (let i = 1; i <= totalPages; i++) pages.push(i);
+    return pages;
+  }
+
+  pages.push(1);
+
+  if (currentPage > 3) {
+    pages.push("...");
+  }
+
+  const start = Math.max(2, currentPage - 1);
+  const end = Math.min(totalPages - 1, currentPage + 1);
+
+  for (let i = start; i <= end; i++) {
+    pages.push(i);
+  }
+
+  if (currentPage < totalPages - 2) {
+    pages.push("...");
+  }
+
+  pages.push(totalPages);
+
+  return pages;
+}
