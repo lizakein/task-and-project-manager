c78f670e5cec369c273a0a5635d2e0a2740d2321
lizakein <lizakein@mail.ru>
2026-03-30 15:26:21 +0300

    Add color checking in getTagStyle and borderColor in return

--- src/utils/tagUtils.ts
+++ src/utils/tagUtils.ts
@@ -3,8 +3,17 @@ import { TAG_COLORS } from "@constants/tagColors"
 export function getTagStyle(colorKey: keyof typeof TAG_COLORS) {
   const color = TAG_COLORS[colorKey];
 
+  if (!color) {
+    return {
+      color: "#000",
+      backgroundColor: "#fff",
+      borderColor: "#00000020",
+    }
+  }
+
   return {
     color: color.text,
-    backgroundColor: color.background
+    backgroundColor: color.background,
+    borderColor: color.border,
   };
 }
