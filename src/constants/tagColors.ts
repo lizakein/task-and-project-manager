82aec867c9a986dc07819e119796285e5b930a27
lizakein <lizakein@mail.ru>
2026-04-02 18:51:46 +0300

    Refactor TAG_COLORS structure and simplify getTagStyle function

--- src/constants/tagColors.ts
+++ src/constants/tagColors.ts
@@ -1,8 +1,8 @@
 export const TAG_COLORS = {
-  blue: { text: "#123CA6", background: "#E2EAFF", border: "#123CA620" },
-  red: { text: "#A60717", background: "#FFF0F1", border: "#A6071720" },
-  green: { text: "#035C41", background: "#E9FFF8", border: "#035C4120" },
-  orange: { text: "#884301", background: "#FFF8F2", border: "#b8692020" },
-  pink: { text: "#960549", background: "#ffeaf4ff", border: "#b41c6320" },
-  purple: { text: "#6D0596", background: "#fdeefdff", border: "#9122bd20" },
+  blue: "#123CA6",
+  red: "#A60717",
+  green: "#035C41",
+  orange: "#884301",
+  pink: "#960549",
+  purple: "#6D0596",
 }
--- src/utils/tagUtils.ts
+++ src/utils/tagUtils.ts
@@ -1,19 +1,7 @@
 import { TAG_COLORS } from "@constants/tagColors"
 
 export function getTagStyle(colorKey: keyof typeof TAG_COLORS) {
-  const color = TAG_COLORS[colorKey];
-
-  if (!color) {
-    return {
-      color: "#000",
-      backgroundColor: "#fff",
-      borderColor: "#00000020",
-    }
-  }
-
   return {
-    color: color.text,
-    backgroundColor: color.background,
-    borderColor: color.border,
+    color: TAG_COLORS[colorKey] || "#000"
   };
 }
