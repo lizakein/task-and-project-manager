0cb9e7ad567b0b0710dc8cc8d32d5618db82a0af
lizakein <lizakein@mail.ru>
2026-04-09 13:33:12 +0300

    Add types alias

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
-import { fileURLToPath } from 'url'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -20,6 +20,7 @@ export default defineConfig({
       "@hooks": path.resolve(__dirname, "./src/hooks"),
       "@constants": path.resolve(__dirname, "./src/constants"),
       "@utils": path.resolve(__dirname, "./src/utils"),
+      "@app-types": path.resolve(__dirname, "./src/types"),
     },
   },
-})
+});
